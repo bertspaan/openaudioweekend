b15c463935ad8622d256d0d9899e5ee07fead482
Bert Spaan <bert.spaan@gmail.com>
2016-06-26 14:20:04 -0400

    map server + viewer

---
+++ map/README.md
@@ -0,0 +1,13 @@
+# Map
+
+Web client with map viewer, connects with `transcript-to-geojson` module.
+
+Uses [Fetch](http://caniuse.com/#feat=fetch), needs Chrome or Firefox.
+
+Install:
+
+    npm Install
+
+Run:
+
+    node index.js
---
+++ map/public/js/map.js
@@ -0,0 +1,63 @@
+var map = L.map('map').setView([40.695,-73.954], 10);
+
+L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+  subdomains: 'abcd',
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+})
+.addTo(map)
+
+var geojsonLayer = L.geoJson(null, {
+  pointToLayer: function(feature, latlon) {
+    return L.circleMarker(latlon, {
+      radius: 8,
+      fillColor: 'red',
+      color: 'red',
+      weight: 1,
+      opacity: 1,
+      opacity: 0.8,
+      fillOpacity: 0.8
+    })
+  }
+}).addTo(map)
+
+function checkStatus (response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
+
+function parseJSON (response) {
+  return response.json()
+}
+
+function transcriptToGeojson (transcript, callback) {
+  fetch('/transcript', {
+    method: 'POST',
+    body: transcript
+  })
+  .then(checkStatus)
+  .then(parseJSON)
+  .then(function(geojson) {
+    callback(null, geojson)
+  }).catch(function(error) {
+    callback(error)
+  })
+}
+
+document.getElementById('submit')
+  .addEventListener('click', () => {
+    const transcript = document.getElementById('editor').value
+    transcriptToGeojson(transcript, function(err, geojson) {
+      if (err) {
+        console.error(err.message)
+      } else {
+        geojsonLayer.clearLayers()
+        geojsonLayer.addData(geojson)
+        map.fitBounds(geojsonLayer.getBounds())
+      }
+    })
+  })
